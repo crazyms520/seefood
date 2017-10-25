8f095f9086f87d3ac5cb6f7d05c878d2a5002035
Ubuntu <ubuntu@ip-172-31-31-164.ap-northeast-1.compute.internal>
2017-10-25 07:24:33 +0000

    test database success

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -20,12 +20,11 @@ class Welcome extends CI_Controller {
 	 */
 	public function index()
 	{
-		$tables = $this->db->list_tables();
-		
-		foreach ($tables as $table)
-		{
-		   echo $table;
+		if ($this->db->table_exists('table_name')) {
+   			echo "yes";
+		} else {
+			echo "no";
 		}
-		// $this->load->view('welcome_message');
+		$this->load->view('welcome_message');
 	}
 }
